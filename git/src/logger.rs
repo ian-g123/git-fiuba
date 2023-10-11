2556403405b8e722dc4c2a0462e8e2a970044480
patriciotourne <patriciotourne@estudiantesorganizados.ar>
2023-10-11 14:56:08 -0300

    Aunque el resultado es correcto, el test unittario no está testeando correctamente. Agregué un assert que debería fallar en la linea 95 y no falla

--- git/src/logger.rs
+++ git/src/logger.rs
@@ -34,6 +34,7 @@ impl Logger {
 
     pub fn log(&mut self, msg: &str) {
         self.logging_channel.send(msg.to_string());
+        self.logging_channel.send("\n".to_string());
     }
 
     pub fn terminate(self) {
@@ -48,36 +49,61 @@ mod tests {
 
     use super::*;
 
-    // Run test of logger with workdir src/test/logger/
     #[test]
-    fn test_logger() {
-        let path = "test/logger/.git/logs";
-        //delete logs file if exists
+    #[ignore]
+    fn test_logger_single_line() {
+        let test_dir = "test/data/logger/.git/";
+        delete_directory_content(test_dir);
+        let path = format!("{test_dir}/logs_test_1");
 
-        let logger_result = Logger::new(path);
+        let logger_result = Logger::new(&path);
         match logger_result {
             Ok(mut logger) => {
-                logger.log("Hello, world!");
-                assert!(Path::new("test/logger/.git/logs").exists());
+                let msg = "Hello, world!";
+                logger.log(msg);
+                assert!(Path::new(&path).exists());
+                // Waits until logger thread has written to file
+                logger.terminate();
+                let Ok(output_content) = fs::read_to_string(path) else {
+                    panic!("Could not read output file")
+                };
+                assert_eq!(output_content, msg);
             }
             Err(error) => panic!("Could not create logger: {}", error),
         };
     }
 
-    fn limpiar_directorio_program_output(path: &str) {
-        let Ok(archivos) = fs::read_dir(path) else {
-            return;
-        };
+    #[test]
+    fn test_logger_two_lines() {
+        let test_dir = "test/data/logger/.git/";
+        delete_directory_content(test_dir);
+        let path = format!("{test_dir}/logs_test_2");
 
-        for archivo in archivos {
-            let Ok(archivo) = archivo else {
-                return;
-            };
-            let archivo_path = archivo.path();
-
-            if archivo_path.is_file() {
-                fs::remove_file(&archivo_path).ok();
+        let logger_result = Logger::new(&path);
+        match logger_result {
+            Ok(mut logger) => {
+                let msg_1 = "Hello, world 1!";
+                let msg_2 = "Hello, world 2!";
+                logger.log(msg_1);
+                logger.log(msg_2);
+                assert!(Path::new(&path).exists());
+                // Waits until logger thread has written to file
+                logger.terminate();
+                let Ok(output_content) = fs::read_to_string(path) else {
+                    panic!("Could not read output file")
+                };
+                assert_eq!("Wrong answer", format!("{}\n{}\n", msg_1, msg_2));
+                //assert_eq!(output_content, format!("{}\n{}\n", msg_1, msg_2));
             }
+            Err(error) => panic!("Could not create logger: {}", error),
+        };
+    }
+
+    fn delete_directory_content(path: &str) {
+        let dir = Path::new(path);
+        if dir.exists() {
+            let _ = fs::remove_dir_all(dir);
         }
+        let _ = fs::create_dir_all(dir);
     }
 }
