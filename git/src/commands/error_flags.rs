473b8a8872d492d0f048ac0cf436226073b6bb18
Patricio Tourne Passarino <ptourne@fi.uba.ar>
2023-10-12 12:58:28 -0300

    [FEAT] Integración en main

--- git/src/commands/error_flags.rs
+++ git/src/commands/error_flags.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum ErrorFlags {
     CommandName,
@@ -6,4 +8,17 @@ pub enum ErrorFlags {
     FileNotFound,
     FileReadError,
     ObjectTypeError,
+}
+
+impl fmt::Display for ErrorFlags {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ErrorFlags::CommandName => write!(f, "El nombre del comando no es válido"),
+            ErrorFlags::WrongFlag => write!(f, "La flag no es válida"),
+            ErrorFlags::InvalidArguments => write!(f, "Argumentos inválidos"),
+            ErrorFlags::FileNotFound => write!(f, "Archivo no encontrado"),
+            ErrorFlags::FileReadError => write!(f, "Error leyendo archivo"),
+            ErrorFlags::ObjectTypeError => write!(f, "Tipo de objeto no válido"),
+        }
+    }
 }
