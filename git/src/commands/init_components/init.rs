9c4f82fe0a254a4318392a42ef676d867f6fa762
Patricio Tourne Passarino <ptourne@fi.uba.ar>
2023-10-13 13:40:24 -0300

    [REF] Variable names

--- git/src/commands/init_components/init.rs
+++ git/src/commands/init_components/init.rs
@@ -1,54 +1,53 @@
-use std::fs::{File, self};
-use std::io::{Read, Write, self};
-use std::{str, env};
+use std::fs::{self, File};
+use std::io::{Read, Write};
+use std::{env, str};
 
-
-use crate::commands::command::Command;
-use crate::commands::error_flags::ErrorFlags;
+use crate::commands::command::{Command, ConfigAdderFunction};
+use crate::commands::command_errors::CommandError;
 use crate::logger::Logger;
 
 /// Commando init
 pub struct Init {
     branch_main: String,
     working_directory: bool,
-    files: Vec<String>,
+    paths: Vec<String>,
 }
 
 impl Command for Init {
     fn run_from(
         name: &str,
         args: &[String],
-        stdin: &mut dyn Read,
+        _stdin: &mut dyn Read,
         output: &mut dyn Write,
-        logger: &mut Logger,
-    ) -> Result<(), ErrorFlags> {
+        _logger: &mut Logger,
+    ) -> Result<(), CommandError> {
         if name != "init" {
-            return Err(ErrorFlags::CommandName);
+            return Err(CommandError::Name);
         }
 
         let mut instance = Self::new(args)?;
 
-        if instance.files.len() == 0 {
-            let current_dir = env::current_dir().map_err(|_| ErrorFlags::InvalidArguments)?;
+        if instance.paths.is_empty() {
+            let current_dir = env::current_dir().map_err(|_| CommandError::InvalidArguments)?;
             let current_dir_display = current_dir.display();
-            instance.files.push(current_dir_display.to_string());
+            instance.paths.push(current_dir_display.to_string());
         }
 
         instance.run(output)?;
         Ok(())
     }
 
-    fn config_adders(&self) -> Vec<fn(&mut Self, usize, &[String]) -> Result<usize, ErrorFlags>> {
+    fn config_adders(&self) -> ConfigAdderFunction<Self> {
         vec![
             Self::add_bare_config,
             Self::add_branch_config,
-            Self::add_file_config,
+            Self::add_path_config,
         ]
     }
 }
 
 impl Init {
-    fn new(args: &[String]) -> Result<Self, ErrorFlags> {
+    fn new(args: &[String]) -> Result<Self, CommandError> {
         let mut init = Self::new_default();
         init.config(args)?;
         Ok(init)
@@ -57,18 +56,14 @@ impl Init {
     fn new_default() -> Self {
         Self {
             branch_main: "main".to_string(),
-            working_directory : true,
-            files: Vec::<String>::new(),
+            working_directory: true,
+            paths: Vec::<String>::new(),
         }
     }
 
-    fn add_bare_config(
-        init: &mut Init,
-        i: usize,
-        args: &[String],
-    ) -> Result<usize, ErrorFlags> {
+    fn add_bare_config(init: &mut Init, i: usize, args: &[String]) -> Result<usize, CommandError> {
         if args[i] != "--bare" {
-            return Err(ErrorFlags::WrongFlag);
+            return Err(CommandError::WrongFlag);
         }
         init.working_directory = false;
         Ok(i + 1)
@@ -78,12 +73,12 @@ impl Init {
         init: &mut Init,
         i: usize,
         args: &[String],
-    ) -> Result<usize, ErrorFlags> {
+    ) -> Result<usize, CommandError> {
         if args[i] != "-b" {
-            return Err(ErrorFlags::WrongFlag);
+            return Err(CommandError::WrongFlag);
         }
-        if args.len()<= i+1 {
-            return Err(ErrorFlags::InvalidArguments);
+        if args.len() <= i + 1 {
+            return Err(CommandError::InvalidArguments);
         }
 
         init.branch_main = args[i + 1].clone();
@@ -91,102 +86,94 @@ impl Init {
         Ok(i + 2)
     }
 
-    fn add_file_config(
-        init: &mut Init,
-        i: usize,
-        args: &[String],
-    ) -> Result<usize, ErrorFlags> {
+    fn add_path_config(init: &mut Init, i: usize, args: &[String]) -> Result<usize, CommandError> {
         if Self::is_flag(&args[i]) {
-            return Err(ErrorFlags::WrongFlag);
-        }
-        if init.files .len() > 1 {
-            return Err(ErrorFlags::InvalidArguments);
+            return Err(CommandError::WrongFlag);
         }
         let path_aux = args[i].clone();
         let root = if path_aux.starts_with('/') {
             path_aux
         } else {
-            let current_dir = env::current_dir().map_err(|_| ErrorFlags::InvalidArguments)?;
+            let current_dir = env::current_dir().map_err(|_| CommandError::InvalidArguments)?;
             let current_dir_display = current_dir.display();
             format!("{}/{}", current_dir_display, path_aux)
         };
-        init.files.push(root);
+        init.paths.push(root);
 
         Ok(i + 1)
     }
 
-    fn run(&self, output: &mut dyn Write) -> Result<(), ErrorFlags> {
-        for path in &self.files {
+    fn run(&self, output: &mut dyn Write) -> Result<(), CommandError> {
+        for path in &self.paths {
             self.run_for_content(path, output)?;
         }
         Ok(())
     }
 
-    fn run_for_content(&self, file : &String, output: &mut dyn Write) -> Result<(), ErrorFlags> {
-        self.create_dirs(file)?;
-        self.create_files(file)?;
-        let output_text = format!("Initialized empty Git repository in {}", file);
-        let _ = writeln!(output, "{}", output_text );
+    fn run_for_content(&self, path: &String, output: &mut dyn Write) -> Result<(), CommandError> {
+        self.create_dirs(path)?;
+        self.create_files(path)?;
+        let output_text = format!("Initialized empty Git repository in {}", path);
+        let _ = writeln!(output, "{}", output_text);
         Ok(())
     }
 
-    fn create_dirs(&self, file : &String) -> Result<(), ErrorFlags> {
-        if fs::create_dir_all(file).is_err(){
-            return Err(ErrorFlags::InvalidArguments);
+    fn create_dirs(&self, path: &String) -> Result<(), CommandError> {
+        if fs::create_dir_all(path).is_err() {
+            return Err(CommandError::InvalidArguments);
         }
-        let file_aux = if !self.working_directory {
-            file.clone()
+        let path_aux = if !self.working_directory {
+            path.clone()
         } else {
-            format!("{}/.git", file)
+            format!("{}/.git", path)
         };
-        self.create_dir(&file_aux,"objects".to_string())?;
-        self.create_dir(&file_aux,"objects/info".to_string())?;
-        self.create_dir(&file_aux,"objects/pack".to_string())?;
-        self.create_dir(&file_aux,"refs".to_string())?;
-        self.create_dir(&file_aux,"refs/tags".to_string())?;
-        self.create_dir(&file_aux,"refs/heads".to_string())?;
-        self.create_dir(&file_aux,"branches".to_string())?;
-        Ok(())     
+        self.create_dir(&path_aux, "objects".to_string())?;
+        self.create_dir(&path_aux, "objects/info".to_string())?;
+        self.create_dir(&path_aux, "objects/pack".to_string())?;
+        self.create_dir(&path_aux, "refs".to_string())?;
+        self.create_dir(&path_aux, "refs/tags".to_string())?;
+        self.create_dir(&path_aux, "refs/heads".to_string())?;
+        self.create_dir(&path_aux, "branches".to_string())?;
+        Ok(())
     }
 
-    fn create_dir(&self, file: &String , name : String) -> Result<(), ErrorFlags> {
-        if fs::create_dir_all(format!("{}/{}", file, name)).is_ok(){
+    fn create_dir(&self, file: &String, name: String) -> Result<(), CommandError> {
+        if fs::create_dir_all(format!("{}/{}", file, name)).is_ok() {
             Ok(())
         } else {
-            return Err(ErrorFlags::InvalidArguments);
+            Err(CommandError::InvalidArguments)
         }
     }
 
-
-    fn create_files(&self, file : &String) -> Result<(), ErrorFlags> {
-        if fs::create_dir_all(file).is_err(){
-            return Err(ErrorFlags::InvalidArguments);
+    fn create_files(&self, path: &String) -> Result<(), CommandError> {
+        if fs::create_dir_all(path).is_err() {
+            return Err(CommandError::InvalidArguments);
         }
-        let file_aux = if !self.working_directory {
-            file.clone()
+        let path_aux = if !self.working_directory {
+            path.clone()
         } else {
-            format!("{}/.git", file)
+            format!("{}/.git", path)
         };
-        self.create_file(&file_aux,"HEAD".to_string())?;
-        Ok(())     
+        self.create_file(&path_aux, "HEAD".to_string())?;
+        Ok(())
     }
 
-    fn create_file(&self, file: &String, name: String) -> Result<(), ErrorFlags> {
-        if fs::create_dir_all(file).is_ok() {
-            let mut _archivo = match File::create(format!("{}/{}",file, name)) {
+    fn create_file(&self, path: &String, name: String) -> Result<(), CommandError> {
+        if fs::create_dir_all(path).is_ok() {
+            match File::create(format!("{}/{}", path, name)) {
                 Ok(mut archivo) => {
                     let texto = format!("ref: refs/heads/{}", self.branch_main);
-                    let _: Result<(), ErrorFlags> = match archivo.write_all(texto.as_bytes()) {
+                    let _: Result<(), CommandError> = match archivo.write_all(texto.as_bytes()) {
                         Ok(_) => Ok(()),
-                        Err(_) => Err(ErrorFlags::InvalidArguments),
+                        Err(_) => Err(CommandError::InvalidArguments),
                     };
                 }
-                Err(_) => return Err(ErrorFlags::InvalidArguments),
+                Err(_) => return Err(CommandError::InvalidArguments),
             };
         } else {
-            return Err(ErrorFlags::InvalidArguments);
+            return Err(CommandError::InvalidArguments);
         }
-    
+
         Ok(())
     }
 }
--- git/src/lib.rs
+++ git/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod commands {
     pub mod command;
+    pub mod command_errors;
+    pub mod file_compressor;
     pub mod hash_object_components;
     pub mod init_components;
 }
