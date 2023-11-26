9cf49455865e8dde22319761f7925cbb6cd5e02e
Patricio Tourne Passarino <ptourne@fi.uba.ar>
2023-11-26 12:43:01 -0300

    [FIX] Tests dejan archivos basura

--- git/tests/log_integration.rs
+++ git/tests/log_integration.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{fs, process::Command};
 
 mod common {
     pub mod aux;
@@ -20,4 +20,5 @@ fn testx() {
 
     let expected = "commit d9227de41004b217dc80699c93816b9f35127313\nMerge: 2018bd4 3de77b6\nAuthor: melijauregui <mjauregui@fi.uba.ar>\nDate: 1698624225\n\n\tMerge branch 'meli' into ian\ncommit 2018bd4d0e4cb07872f5bc3aac8c69b91db764a9\nAuthor: melijauregui <mjauregui@fi.uba.ar>\nDate: 1698624128\n\n\tcommit5\ncommit 724672fe124e0d27e5e1682e1f31b0a1de17be3b\nAuthor: melijauregui <mjauregui@fi.uba.ar>\nDate: 1698624108\n\n\tcommit4\ncommit 3de77b643006cfa227f77c57cc3eecbd140214c5\nAuthor: melijauregui <mjauregui@fi.uba.ar>\nDate: 1698624067\n\n\tcommit3\ncommit 5e0b574af421a7ada1e47c9fcad216a2c254e458\nAuthor: melijauregui <mjauregui@fi.uba.ar>\nDate: 1698624036\n\n\tcommit2\ncommit 0af2f837bc8dda145c07e8539088a85592cba872\nAuthor: melijauregui <mjauregui@fi.uba.ar>\nDate: 1698623969\n\n\tfirst commit\n";
     assert_eq!(obtained, expected);
+    _ = fs::remove_dir_all(path);
 }
